4b0932444c5907915201d6d43c09ef7a118dd4d7
it-drive-view <it.drive.view@gmail.com>
2024-02-08 11:36:56 +0100

    wip

--- src/test/java/dev/danvega/danson/post/crud/CrudTest2.java
+++ src/test/java/dev/danvega/danson/post/crud/CrudTest2.java
@@ -25,14 +25,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
-class CrudTest {
+class CrudTest2 {
 
 
 
     @Autowired
     PostRepository postRepository;
 
-    Logger logger = LoggerFactory.getLogger(CrudTest.class);
+    Logger logger = LoggerFactory.getLogger(CrudTest2.class);
 
     @Container
     @ServiceConnection
@@ -47,30 +47,32 @@ class CrudTest {
         postgres.followOutput(new Slf4jLogConsumer(LoggerFactory.getLogger("docker logs")));
     }
 
-    @BeforeEach
-    void setUp() {
-        List<Post> posts = List.of(new Post(1,1,"Hello, World!", "This is my first post!",null));
-        postRepository.saveAll(posts);
-    }
-
     @Test
     void connectionEstablished() {
         assertThat(postgres.isCreated()).isTrue();
         assertThat(postgres.isRunning()).isTrue();
     }
 
-//    @Test
-//    void shouldReturnPostByTitle() {
+    @BeforeEach
+    void setUp() {
+        List<Post> posts = List.of(new Post(1,1,"Hello, World!", "This is my first post!",null));
+        postRepository.saveAll(posts);
+    }
+
+
+
+    @Test
+    void shouldReturnPostByTitle() {
 //        Post post = postRepository.findByTitle("Hello, World!").orElseThrow();
 //        assertEquals("Hello, World!", post.title(), "Post title should be 'Hello, World!'");
-//    }
+    }
 
     @Test
     void shouldFindAllPosts() {
-        Post post = new Post(1,1,"title","body",1);
-        postRepository.save(post);
-        Optional<Post> optional = postRepository.findById(1);
-        Assertions.assertTrue(optional.isPresent());
+//        Post post = new Post(1,1,"title","body",1);
+//        postRepository.save(post);
+//        Optional<Post> optional = postRepository.findById(1);
+//        Assertions.assertTrue(optional.isPresent());
     }
 
 }
