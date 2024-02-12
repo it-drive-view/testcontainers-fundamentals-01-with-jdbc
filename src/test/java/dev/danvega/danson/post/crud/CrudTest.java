7174ff51d479d96529e391d3d25f91030bfdccfd
it-drive-view <it.drive.view@gmail.com>
2024-02-12 12:19:20 +0100

    wip

--- src/test/java/dev/danvega/danson/post/crud/CrudTest.java
+++ src/test/java/dev/danvega/danson/post/crud/CrudTest.java
@@ -4,40 +4,39 @@ import dev.danvega.danson.post.Post;
 import dev.danvega.danson.post.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.util.List;
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class Temp {
+class CrudTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    Logger logger = LoggerFactory.getLogger(CrudTest.class);
 
     @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.0");
 
-    @Autowired
-    PostRepository postRepository;
-
-    @Autowired
-    JdbcConnectionDetails jdbcConnectionDetails;
-
+    /* système de log 1
+     */
     @BeforeEach
-    void setUp() {
-        List<Post> posts = List.of(new Post(1,1,"Hello, World!", "This is my first post!",null));
-        postRepository.saveAll(posts);
+    void followContainerLOgs1() throws InterruptedException {
+        postgres.followOutput(new Slf4jLogConsumer(LoggerFactory.getLogger("docker logs")));
     }
 
     @Test
@@ -47,15 +46,13 @@ public class Temp {
     }
 
     @Test
-    void shouldReturnPostByTitle() {
-        Post post = postRepository.findByTitle("Hello, World!").orElseThrow();
-        assertEquals("Hello, World!", post.title(), "Post title should be 'Hello, World!'");
-    }
+    void shouldFindAllPosts() {
 
-    @Test
-    void shouldNotReturnPostWhenTitleIsNotFound() {
-        Optional<Post> post = postRepository.findByTitle("Hello, Wrong Title!");
-        assertFalse(post.isPresent(), "Post should not be present");
+        Post e2 = new Post(2, 1, "Hello, World! 1", "This is my first post!", null);
+        postRepository.save(e2);
+
+        Post post = postRepository.findByTitle("Hello, World! 1").orElseThrow();
+        assertEquals("Hello, World! 1", post.title(), "Post title should be 'Hello, World!'");
     }
 
 }
